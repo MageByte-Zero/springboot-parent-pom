dfe07435665c5c0ff7cb30a6cafc0ee31be74094
zero <zero_develop@163.com>
2022-06-02 11:58:27 +0800

    doc[caffeine]:完善注释

--- caffeine-cache/src/test/java/com/zero/caffeine/cache/test/CaffeineApplicationTests.java
+++ caffeine-cache/src/test/java/com/zero/caffeine/cache/test/CaffeineApplicationTests.java
@@ -25,26 +25,29 @@ public class CaffeineApplicationTests {
 
     @Test
     public void testCache() {
+        // 插入缓存 和数据库
         AddressDTO newInsert = addressService.create("南山大道");
         // 要走缓存
         AddressDTO address = addressService.getAddress(newInsert.getCustomerId());
+        long customerId = 2;
 
         // 第一次未命中缓存，打印 customerId：{} 没有走缓存，开始从数据库查询
-        AddressDTO address2 = addressService.getAddress(2);
+        AddressDTO address2 = addressService.getAddress(customerId);
         // 命中缓存
-        AddressDTO cacheAddress2 = addressService.getAddress(2);
+        AddressDTO cacheAddress2 = addressService.getAddress(customerId);
 
-        addressService.update(2L, "地址 2 被修改");
+        // 更新数据库和缓存
+        addressService.update(customerId, "地址 2 被修改");
 
         // 更新后查询，依然命中缓存
-        AddressDTO hitCache2 = addressService.getAddress(2);
+        AddressDTO hitCache2 = addressService.getAddress(customerId);
         Assert.assertEquals(hitCache2.getAddress(), "地址 2 被修改");
 
         // 删除缓存
-        addressService.delete(2);
+        addressService.delete(customerId);
 
         // 未命中缓存, 从数据库读取
-        AddressDTO hit = addressService.getAddress(2);
+        AddressDTO hit = addressService.getAddress(customerId);
         System.out.println(hit.getCustomerId());
     }
 
