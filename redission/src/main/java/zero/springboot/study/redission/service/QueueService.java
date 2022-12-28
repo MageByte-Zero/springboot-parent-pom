e94f27faa97029cbdc4a118c30145971ecf78174
MageByte <zero_develop@163.com>
2022-12-28 23:38:01 +0800

    fix「Redisson」:List 消息队列 ack

--- redission/src/main/java/zero/springboot/study/redission/service/QueueService.java
+++ redission/src/main/java/zero/springboot/study/redission/service/QueueService.java
@@ -3,8 +3,11 @@ package zero.springboot.study.redission.service;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBlockingDeque;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.queue.DequeMoveArgs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Slf4j
 @Service
@@ -13,7 +16,8 @@ public class QueueService {
     @Autowired
     private RedissonClient redissonClient;
 
-    private static final String REDIS_MQ = "redisMQ";
+    private static final String ORDER_PAY_QUEUE = "order:pay";
+    private static final String ORDER_PAY_BACK_QUEUE = "order:pay:back";
 
     /**
      * 发送消息到队列头部
@@ -21,11 +25,11 @@ public class QueueService {
      * @param message
      */
     public void sendMessage(String message) {
-        RBlockingDeque<String> blockingDeque = redissonClient.getBlockingDeque(REDIS_MQ);
+        RBlockingDeque<String> orderPayQueue = redissonClient.getBlockingDeque(ORDER_PAY_QUEUE);
 
         try {
-            blockingDeque.putFirst(message);
-            log.info("将消息: {} 插入到队列。", message);
+            orderPayQueue.putFirst(message);
+            log.info("将消息: {} 插入到队列 {}。", message, ORDER_PAY_QUEUE);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -35,15 +39,27 @@ public class QueueService {
      * 从队列尾部阻塞当时获取消息
      */
     public void onMessage() {
-        RBlockingDeque<String> blockingDeque = redissonClient.getBlockingDeque(REDIS_MQ);
-        while (true) {
-            try {
-                String message = blockingDeque.takeLast();
-                log.info("从队列 {} 中读取到消息：{}.", REDIS_MQ, message);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
 
+        RBlockingDeque<String> orderPayQueue = redissonClient.getBlockingDeque(ORDER_PAY_QUEUE);
+        while (true) {
+            // BLMOVE order:pay order:pay:back RIGHT LEFT 0
+            String message = orderPayQueue.move(Duration.ofSeconds(0), DequeMoveArgs.pollLast()
+                    .addFirstTo(ORDER_PAY_BACK_QUEUE));
+            log.info("从队列 {} 中读取到消息：{}，并把消息复制到 {} 队列.", ORDER_PAY_QUEUE, message, ORDER_PAY_BACK_QUEUE);
+
+            // 消费正常，从 ORDER_PAY_BACK_QUEUE 删除这条消息，LREM order:pay:back 0 message
+            removeBackQueueMessage(message, ORDER_PAY_BACK_QUEUE);
         }
+    }
+
+    /**
+     * 从队列中删除消息
+     * @param message
+     * @param queueName
+     */
+    private void removeBackQueueMessage(String message, String queueName) {
+        RBlockingDeque<String> orderPayBackDeque = redissonClient.getBlockingDeque(queueName);
+        boolean remove = orderPayBackDeque.remove(message);
+        log.info("消费正常，删除队列 {} 的消息 {}。", queueName, message);
     }
 }
