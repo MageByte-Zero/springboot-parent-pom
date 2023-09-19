f1331db876ac99ee7ae7b442e60463b84c3c35aa
MageByte <zero_develop@163.com>
2023-09-19 20:42:43 +0800

    feat(redis): RBloomFilter

--- redission/src/main/java/zero/springboot/study/redission/service/BloomFilterService.java
+++ redission/src/main/java/zero/springboot/study/redission/service/BloomFilterService.java
@@ -19,10 +19,46 @@ public class BloomFilterService {
      * @param <T>
      * @return
      */
-    public <T> RBloomFilter<T> create(String filterName, long expectedInsertions, double falseProbability) {
+    public <T> boolean create(String filterName, long expectedInsertions, double falseProbability) {
         RBloomFilter<T> bloomFilter = redissonClient.getBloomFilter(filterName);
-        bloomFilter.tryInit(expectedInsertions, falseProbability);
-        return bloomFilter;
+        return bloomFilter.tryInit(expectedInsertions, falseProbability);
+    }
+
+    /**
+     * 添加元素到布隆过滤器
+     * @param filterName
+     * @param element
+     * @return true if element has been added successfully false if element is already present
+     */
+    public <T> boolean add(String filterName, T element) {
+        RBloomFilter<T> bloomFilter = redissonClient.getBloomFilter(filterName);
+        return bloomFilter.add(element);
+    }
+
+    /**
+     * 判断元素是否存在布隆过滤器中
+     * @param filterName
+     * @param element
+     * @return true if element is present false if element is not present
+     */
+    public <T> boolean contains(String filterName, T element) {
+        RBloomFilter<T> bloomFilter = redissonClient.getBloomFilter(filterName);
+        return bloomFilter.contains(element);
+    }
+
+    /**
+     * 计算布隆过滤器的元素个数
+     * @param filterName
+     * @return
+     */
+    public <T> long count(String filterName) {
+        RBloomFilter<T> bloomFilter = redissonClient.getBloomFilter(filterName);
+        return bloomFilter.count();
+    }
+
+    public <T> boolean delete(String filterName) {
+        RBloomFilter<T> bloomFilter = redissonClient.getBloomFilter(filterName);
+        return bloomFilter.delete();
     }
 
 }
--- redission/src/test/java/zero/springboot/study/redission/test/BloomFilterTest.java
+++ redission/src/test/java/zero/springboot/study/redission/test/BloomFilterTest.java
@@ -3,7 +3,6 @@ package zero.springboot.study.redission.test;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.redisson.api.RBloomFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,23 +21,30 @@ public class BloomFilterTest {
     public void testBloomFilter() {
         long expectedInsertions = 10000L;
         double falseProbability = 0.01;
-        RBloomFilter<Long> bloomFilter = bloomFilterService.create("ipBlackList", expectedInsertions, falseProbability);
+        String filterName = "ipBlackList";
+
+        boolean createFlag = bloomFilterService.create(filterName, expectedInsertions, falseProbability);
+        log.info("Created bloom filter {}", createFlag);
 
         // 布隆过滤器增加元素
         for (long i = 0; i < expectedInsertions; i++) {
-            bloomFilter.add(i);
+            bloomFilterService.add(filterName, i);
         }
-        long elementCount = bloomFilter.count();
+
+        long elementCount = bloomFilterService.count(filterName);
         log.info("elementCount = {}.", elementCount);
 
         // 统计误判次数
         int count = 0;
         for (long i = expectedInsertions; i < expectedInsertions * 2; i++) {
-            if (bloomFilter.contains(i)) {
+            boolean contains = bloomFilterService.contains(filterName, i);
+            if (contains) {
                 count++;
             }
         }
         log.info("误判次数 = {}.", count);
-        bloomFilter.delete();
+
+        bloomFilterService.delete(filterName);
+
     }
 }
